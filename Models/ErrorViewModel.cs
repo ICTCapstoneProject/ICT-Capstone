ae66fcfdbe832a8d5ff57cdb35277fe0e001894f
9Quiescent <Dkalongonda@gmail.com>
2025-03-24 21:03:56 +1030

    Initial commit - ASP.NET MVC Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ProjectManagerMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
