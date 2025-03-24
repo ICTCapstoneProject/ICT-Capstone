972dd9495c74656a6581b85dc6a16743963d0dd1
9Quiescent <Dkalongonda@gmail.com>
2025-03-24 23:44:25 +1030

    Modify Navbar

--- obj/Debug/net8.0/ProjectManagerMvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjectManagerMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManagerMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96ce7b6bdbc650c02542760784991fe95e70563f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bff936c6c0a852057f8aeadcea0ecc88935a8cc5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManagerMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManagerMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
