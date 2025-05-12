3a3f9c99e027947ad2f9a9a01962d19e3f60e9f3
Yi Han <hanyy032@mymail.unisa.edu.au>
2025-05-12 19:32:44 +0930

    Improved My Proposals page looking
    improved My Proposals page looking by display each proposals as its own separate table instead of make one table display all proposals together.

--- obj/Debug/net8.0/ProjectManagerMvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/ProjectManagerMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjectManagerMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed043ffd620b70124046ca23fd84fcd5b2ffd20f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f54e345f227676b4abc7bcfbc93462fbbc29f6c6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjectManagerMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjectManagerMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
