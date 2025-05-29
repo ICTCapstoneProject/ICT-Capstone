51dc885861e756108d481151414475d484dd1976
Alan Donehue <alan.r.donehue@gmail.com>
2025-05-29 13:44:20 +0930

    - Add reference to MyProposalViewModel to reduce code duplication. - Remove unnecessary references

--- Models/ProposalReviewViewModel.cs
+++ Models/ProposalReviewViewModel.cs
@@ -1,17 +1,5 @@
-public class ProposalReviewViewModel
+public class ProposalReviewViewModel : MyProposalViewModel
 {
-    public int Id { get; set; }
-    public string Title { get; set; }
-    public string Synopsis { get; set; }
-    public string Method { get; set; }
-    public string Outcomes { get; set; }
-    public string Resources { get; set; }
-    public string EthicalConsiderations { get; set; }
-    public string Milestones { get; set; }
-    public DateTime EstimatedCompletionDate { get; set; }
-    public string SubmittedByName { get; set; }
-    public string StatusName { get; set; }
-    public string ProjectLevel { get; set; }
 
     public Dictionary<string, string> FieldComments { get; set; } = new();
 
