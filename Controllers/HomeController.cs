1c31ae09ba6e00a4dfa952b2c904e688f1fba6f1
9Quiescent <Dkalongonda@gmail.com>
2025-06-01 12:27:26 +0930

    Add commitee approval confirmation modal

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -89,7 +89,7 @@ public class HomeController : Controller
                 {
                     "submitted"         => $"Proposal #{x.ProposalId}, <strong>'{x.Title}'</strong> was <span class='text-success'>submitted</span> by {x.UserName}",
                     "modified"          => $"Proposal #{x.ProposalId}, <strong>'{x.Title}'</strong> was <span class='text-warning'>modified</span> by {x.UserName}",
-                    "committee_approve" => $"Proposal #{x.ProposalId}, <strong>'{x.Title}'</strong> was granted <span class='text-info'>committee approval</span> from {x.UserName}",
+                    "approve" => $"Proposal #{x.ProposalId}, <strong>'{x.Title}'</strong> was granted <span class='text-info'>committee approval</span> from {x.UserName}",
                     "chair_approve"     => $"Proposal #{x.ProposalId}, <strong>'{x.Title}'</strong> was granted <span class='text-primary'>chair approval</span> from {x.UserName}",
                     "marked_complete"   => $"Proposal #{x.ProposalId}, <strong>'{x.Title}'</strong> was <span style='color: #b5e6b6;'>marked complete</span> by {x.UserName}",
                     "commenced"         => $"Proposal #{x.ProposalId}, <strong>'{x.Title}'</strong> was set to <span style='color: #a259e6;'>commence</span> by {x.UserName}",
