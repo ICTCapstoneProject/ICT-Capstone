8d78637c1567e488ecf7580fbe9fa4cd4138eb84
Alan Donehue <alan.r.donehue@gmail.com>
2025-06-04 22:06:10 +0930

    Add Column atrribute names

--- Models/Comment.cs
+++ Models/Comment.cs
@@ -1,28 +1,30 @@
 using System;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace FSSA.Models
 {
     public class Comment
     {
+        [Column("comment_id")]
         public int Id { get; set; }
 
-        [Required]
+        [Column("comment")]
         public string Content { get; set; } = string.Empty;
 
+        [Column("timestamp")]
         public DateTime Timestamp { get; set; } = DateTime.Now;
 
         // Foreign Keys
+        [Column("review_id")]
         public int ProposalId { get; set; }
 
         [ForeignKey("ProposalId")]
         public Proposal? Proposal { get; set; }
 
+        [Column("commenter")]
         public int UserId { get; set; }
 
         [ForeignKey("UserId")]
         public User? User { get; set; }
     }
-
 }
