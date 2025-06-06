bc621486202c30bdcbff49e676cc5b7596cbc73d
Alan Donehue <alan.r.donehue@gmail.com>
2025-06-06 11:18:44 +0930

    Add Collection property for Comment objects

--- Models/Proposal.cs
+++ Models/Proposal.cs
@@ -67,14 +67,17 @@ namespace FSSA.Models
         [Required]
         [Column("submitted_by")]
         public int SubmittedBy { get; set; }
-        
+
         [Required(ErrorMessage = "A Lead Researcher is required.")]
         [Column("lead_researcher_id")]
         public int LeadResearcherId { get; set; }
 
-         [InverseProperty("Proposal")]
+        [InverseProperty("Proposal")]
         public virtual ICollection<Attachment> Attachments { get; set; } = new List<Attachment>();
         [ValidateNever]
-        public virtual ICollection<Notification> Notifications { get; set; }    
+        public virtual ICollection<Notification> Notifications { get; set; }  
+
+        public ICollection<Comment> Comments { get; set; }
+  
     }
 }
